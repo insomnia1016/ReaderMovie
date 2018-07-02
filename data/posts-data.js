7fec377e6fe32e8999c2500dc057cd5af08edd5b
insomnia1016 <gaoyuejianwu@qq.com>
2018-07-03 01:22:53 +0800

    完善音乐播放功能

--- data/posts-data.js
+++ data/posts-data.js
@@ -12,11 +12,12 @@ var local_database = [{
     detail: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，壳凸红脂块块香”；在《世说新语》里，晋毕卓更是感叹“右手持酒杯，左手持蟹螯，拍浮酒船中，便足了一生矣。”漫漫人生长路，美食与爱岂可辜负？于是作为一个吃货，突然也很想回味一下属于我的味蕾记忆。记忆中的秋蟹，是家人的味道，弥漫着浓浓的亲情。\n\n是谁来自山川湖海，却囿于昼夜，厨房与爱？ 是母亲，深思熟虑，聪明耐心。吃蟹前，总会拿出几件工具，煞有介事而乐此不疲。告诉我们螃蟹至寒，需要佐以姜茶以祛寒，在配备的米醋小碟里，亦添入姜丝与紫苏，前者驱寒后者增香。泡好菊花茶，岁月静好，我们静等。",
     postId: 0,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=142604.mp3",
       title: "夜夜夜夜-齐秦",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
     }
-  }, {
+  },
+  {
     title: "比利·林恩的中场故事",
     content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
     imgSrc: "/images/post/bl.png",
@@ -30,7 +31,7 @@ var local_database = [{
     avatar: "/images/avatar/1.png",
     postId: 1,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=108220.mp3",
       title: "鬼迷心窍-李宗盛",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
     }
@@ -50,7 +51,7 @@ var local_database = [{
     avatar: "/images/avatar/3.png",
     postId: 2,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=27538254.mp3",
       title: "女儿情-万晓利",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
     }
@@ -69,7 +70,7 @@ var local_database = [{
     avatar: "/images/avatar/5.png",
     postId: 3,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=108119.mp3",
       title: "恋恋风尘-老狼",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
     }
@@ -88,7 +89,7 @@ var local_database = [{
     avatar: "../../../images/avatar/3.png",
     postId: 4,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=188204.mp3",
       title: "沉默是金-张国荣",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
     }
@@ -107,13 +108,13 @@ var local_database = [{
     avatar: "../../../images/avatar/5.png",
     postId: 5,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+      url: "http://music.163.com/song/media/outer/url?id=152428.mp3",
       title: "朋友-谭咏麟",
       coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
     }
   },
 ]
 
-module.exports={
-  postList:local_database
+module.exports = {
+  postList: local_database
 }
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,44 +1,72 @@
-
 var postsData = require('../../../data/posts-data.js')
-
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    isPlayingMusic:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
     var postId = options.id
     this.data.currentPostId = postId
     var postData = postsData.postList[postId]
     this.setData({
-      postData:postData
+      postData: postData
     })
-    
+
     var postsCollected = wx.getStorageSync("posts_collected")
-    if(postsCollected){
+    if (postsCollected && postCollected) {
       var postCollected = postsCollected[postId]
       this.setData({
-        collected:postCollected
+        collected: postCollected
       })
-    }else{
-      var postsCollected ={}
+    } else {
+      var postsCollected = {}
       postsCollected[postId] = false
       wx.setStorageSync("posts_collected", postsCollected)
     }
+    if (app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId === postId){
+      this.setData({
+        isPlayingMusic:true
+      })
+    }
+    this.setMusicMonitor()
   },
-  onCollectionTap:function(event){
+  setMusicMonitor:function(){
+    var that = this
+    wx.onBackgroundAudioPlay(function () {
+      that.setData({
+        isPlayingMusic: true
+      })
+      app.globalData.g_isPlayingMusic = true
+      app.globalData.g_currentMusicPostId = that.data.currentPostId
+    })
+    wx.onBackgroundAudioPause(function () {
+      that.setData({
+        isPlayingMusic: false
+      })
+      
+      app.globalData.g_isPlayingMusic = false
+      app.globalData.g_currentMusicPostId = null
+    })
+    wx.onBackgroundAudioStop(function(){
+      that.setData({
+        isPlayingMusic:false
+      })
+      app.globalData.g_isPlayingMusic = false
+    })
+  },
+  onCollectionTap: function(event) {
     //this.getPostsCollectedAsy()
     this.getPostsCollectedSyc()
   },
-  onShareTap:function(){
+  onShareTap: function() {
     var itemList = [
       "分享到微信好友",
       "分享到朋友圈",
@@ -48,34 +76,57 @@ Page({
     wx.showActionSheet({
       itemList: itemList,
       itemColor: "#405f80",
-      success:function(res){
-        if(res.cancel){
+      success: function(res) {
+        if (res.cancel) {
           console.log('用户点击了取消')
         }
         wx.showModal({
-          title: '用户'+itemList[res.tapIndex],
-          content: '用户是否取消'+res.cancel+'现在还无法实现分享功能，api不支持。',
+          title: '用户' + itemList[res.tapIndex],
+          content: '用户是否取消' + res.cancel + '现在还无法实现分享功能，api不支持。',
         })
       },
-      fail:function(res){
+      fail: function(res) {
         //点击“取消”按钮，errMsg显示“showActionSheet:fail cancel”
         console.log(res.errMsg)
       }
     })
   },
-  getPostsCollectedSyc:function(){
+  onMusicTap:function(event){
+    var currentPostId = this.data.currentPostId
+    var musicData = postsData.postList[currentPostId]
+    var isPlayingMusic = this.data.isPlayingMusic
+    if(isPlayingMusic){
+      wx.pauseBackgroundAudio()
+      this.setData({
+        isPlayingMusic:false
+      })
+
+    }else{
+      wx.playBackgroundAudio({
+        dataUrl: musicData.music.url,
+        title: musicData.music.title,
+        coverImgUrl: musicData.music.coverImg,
+      })
+      this.setData({
+        isPlayingMusic:true
+      })
+     
+    }
+    
+  },
+  getPostsCollectedSyc: function() {
     var postsCollected = wx.getStorageSync('posts_collected');
     var postCollected = postsCollected[this.data.currentPostId]
     postCollected = !postCollected
     postsCollected[this.data.currentPostId] = postCollected
     this.showToast(postCollected, postsCollected)
   },
-  
-  getPostsCollectedAsy:function(){
+
+  getPostsCollectedAsy: function() {
     var that = this
     wx.getStorage({
       key: "posts_collected",
-      success: function (res) {
+      success: function(res) {
         var postsCollected = res.data
         var postCollected = postsCollected[that.data.currentPostId]
         postCollected = !postCollected
@@ -83,15 +134,15 @@ Page({
         that.showToast(postCollected, postsCollected)
       }
     })
-  },  
-  showModel: function (postCollected, postsCollected) {
+  },
+  showModel: function(postCollected, postsCollected) {
     var that = this
     wx.showModal({
       title: '收藏',
-      content: postCollected?'收藏该文章?':'取消收藏该文章?',
-      confirmColor:'#405f80',
-      success:function(res){
-        if(res.confirm){
+      content: postCollected ? '收藏该文章?' : '取消收藏该文章?',
+      confirmColor: '#405f80',
+      success: function(res) {
+        if (res.confirm) {
           wx.setStorageSync("posts_collected", postsCollected)
           that.setData({
             collected: postCollected
@@ -102,7 +153,7 @@ Page({
     })
   },
 
-  showToast: function (postCollected, postsCollected){
+  showToast: function(postCollected, postsCollected) {
     wx.setStorageSync("posts_collected", postsCollected)
     this.setData({
       collected: postCollected
@@ -113,55 +164,5 @@ Page({
       icon: "success"
     })
   },
-  
 
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
