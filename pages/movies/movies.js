c3d78a682621900cd81877759da49483f1f0c3af
insomnia1016 <gaoyuejianwu@qq.com>
2018-07-04 13:50:51 +0800

    电影首页完成，动态从豆瓣api读取电影信息

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,5 @@
 // pages/movies/movies.js
+var util = require('../../utils/util.js')
 var app = getApp()
 Page({
 
@@ -13,24 +14,50 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters"
-    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon"
-    var top250Url = app.globalData.doubanBase + "/v2/movie/top250"
-    this.getMovieListData(inTheatersUrl)
-    this.getMovieListData(comingSoonUrl)
-    this.getMovieListData(top250Url)
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters?start=0&count=3"
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon?start=0&count=3"
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250?start=0&count=3"
+    this.getMovieListData(inTheatersUrl,'inTheaters','正在热映')
+    this.getMovieListData(comingSoonUrl,'comingSoon','即将上映')
+    this.getMovieListData(top250Url,'top250','豆瓣top250')
   },
 
-  getMovieListData: function(url) {
+  getMovieListData: function (url, settedKey, category) {
+    var that = this
     wx.request({
       url: url,
       success: function(res) {
-        console.log(res)
+        console.log(res.data)
+        that.processDouBanData(res.data, settedKey,category)
       },
       fail: function(error) {
         console.log(error)
       }
     })
+  },
+  processDouBanData: function (moivesDouBan, settedKey, category) {
+    var movies = []
+    for (var idx in moivesDouBan.subjects) {
+      var movie = moivesDouBan.subjects[idx]
+      var title = movie.title
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + '...'
+      }
+      var temp = {
+        stars: util.convertToStarsArray(movie.rating.stars),
+        title: title,
+        coverImg: movie.images.large,
+        movieId: movie.id,
+        average: movie.rating.average
+      }
+      movies.push(temp)
+    }
+    var readyData = {}
+    readyData[settedKey] = {
+      movies:movies,
+      category: category,
+    }
+    this.setData(readyData)
   }
 
 })
